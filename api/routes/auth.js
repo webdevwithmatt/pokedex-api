810a7017366eed89919cb5ba73a56c4cdfb9d5de
Matt Rinker <mrinker@reliaquest.com>
2021-03-20 12:45:06 -0600

    Add login endpoint and helper functions (video 12)

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,6 +4,78 @@ const { bcrypt, jwt, Sequelize } = envVars.requires;
 const { Op } = Sequelize;
 const { promisify } = require('util');
 
+const expiresInMinutes = 30;
+
+const generateAccessToken = (payload) => promisify(jwt.sign)(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: `${expiresInMinutes}m` });
+
+const generateRefreshToken = (payload) => promisify(jwt.sign)(payload, process.env.REFRESH_TOKEN_SECRET);
+
+const getExpiresAt = (minutes) => {
+    let expiresAt = new Date();
+    return new Date(expiresAt.getTime() + (minutes * 60 * 1000));
+};
+
+authServer.post('/login', async (req, res) => {
+    if (!req.body.username && !req.body.email) {
+        return res.status(401).send({
+            message: 'There was a problem logging in. Please check to make sure your email and password are correct and try again.',
+        });
+    }
+
+    const trainer = await models.Trainer.findOne({
+        where: {
+            [Op.and]: {
+                ...(req.body.email ? { email: req.body.email } : {}),
+                ...(req.body.username ? { username: req.body.username } : {}),
+            }
+        },
+        raw: true,
+    });
+
+    if (!trainer) {
+        return res.status(401).send({
+            message: 'There was a problem logging in. Please check to make sure your email and password are correct and try again.',
+        });
+    }
+
+    try {
+        if (!(await bcrypt.compare(req.body.password, trainer.password))) {
+            return res.status(401).send({
+                message: 'There was a problem logging in. Please check to make sure your email and password are correct and try again.',
+            });
+        }
+
+        accessToken = await generateAccessToken(trainer);
+        refreshToken = await generateRefreshToken(trainer);
+
+        const salt = await bcrypt.genSalt();
+        const hashedRefreshToken = await bcrypt.hash(refreshToken, salt);
+
+        await models.Token.create({
+            trainerId: trainer.id,
+            refreshToken: hashedRefreshToken,
+        });
+    } catch (error) {
+        return res.status(500).send({
+            message: 'There was a problem logging in.',
+            error,
+        });
+    }
+
+    if (!accessToken || !refreshToken) {
+        return res.status(500).send({
+            message: 'There was a problem logging in.',
+        });
+    }
+
+    return res.send({
+        expiresInSeconds: expiresInMinutes * 60,
+        expiresAt: getExpiresAt(expiresInMinutes),
+        accessToken,
+        refreshToken,
+    });
+});
+
 authServer.post('/register', async (req, res) => {
     const {
         name,
